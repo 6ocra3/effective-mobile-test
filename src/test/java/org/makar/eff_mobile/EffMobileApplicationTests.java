00cc2b49e6a87a6ad65e5377073ec44a7c809065
6ocra3 <6o3kmm@mail.ru>
2024-12-15 10:40:08 +0300

    feat: init project

---
+++ src/test/java/org/makar/eff_mobile/EffMobileApplicationTests.java
@@ -0,0 +1,13 @@
+package org.makar.eff_mobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffMobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
